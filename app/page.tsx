1dac8bf47a497a19e385f8d6ea5c16948f6204f3
aliyorulmazdev <aliyorulmazdev@gmail.com>
2023-12-28 23:07:32 +0300

    Initial

--- app/page.tsx
+++ app/page.tsx
@@ -1,113 +1,250 @@
-import Image from 'next/image'
+"use client";
+import {
+  Card,
+  CardContent,
+  CardDescription,
+  CardFooter,
+  CardHeader,
+  CardTitle,
+} from "@/components/ui/card";
+import { Button } from "@/components/ui/button";
+import { ComboboxDemo } from "./_components/combobox";
+import { useState } from "react";
+import { Switch } from "@/components/ui/switch";
+import { Label } from "@/components/ui/label";
+import { QRCode } from "react-qrcode-logo";
+import { Tabs, TabsContent, TabsList, TabsTrigger } from "@/components/ui/tabs";
+
+import { Input } from "@/components/ui/input";
 
 export default function Home() {
+  const [generateForEveryTable, setGenerateForEveryTable] = useState(false);
+  const [eyeColor, setEyeColor] = useState("#000000");
+  const [eyeRadius, setEyeRadius] = useState(0);
+  const [qrStyle, setQrStyle] = useState<"squares" | "dots" | undefined>(
+    "squares" // Set the initial value based on your preference
+  );
+  const [bgColor, setBgColor] = useState("#FFFFFF");
+  const [fgColor, setFgColor] = useState("#000000");
+
+  const handleSwitchChange = (checked) => {
+    setGenerateForEveryTable(checked);
+  };
+
   return (
-    <main className="flex min-h-screen flex-col items-center justify-between p-24">
-      <div className="z-10 max-w-5xl w-full items-center justify-between font-mono text-sm lg:flex">
-        <p className="fixed left-0 top-0 flex w-full justify-center border-b border-gray-300 bg-gradient-to-b from-zinc-200 pb-6 pt-8 backdrop-blur-2xl dark:border-neutral-800 dark:bg-zinc-800/30 dark:from-inherit lg:static lg:w-auto  lg:rounded-xl lg:border lg:bg-gray-200 lg:p-4 lg:dark:bg-zinc-800/30">
-          Get started by editing&nbsp;
-          <code className="font-mono font-bold">app/page.tsx</code>
-        </p>
-        <div className="fixed bottom-0 left-0 flex h-48 w-full items-end justify-center bg-gradient-to-t from-white via-white dark:from-black dark:via-black lg:static lg:h-auto lg:w-auto lg:bg-none">
-          <a
-            className="pointer-events-none flex place-items-center gap-2 p-8 lg:pointer-events-auto lg:p-0"
-            href="https://vercel.com?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            By{' '}
-            <Image
-              src="/vercel.svg"
-              alt="Vercel Logo"
-              className="dark:invert"
-              width={100}
-              height={24}
-              priority
+    <div className="space-y-4">
+      <div className="text-center">
+        <h1 className="text-2xl font-bold">QR Code Generator</h1>
+      </div>
+      <Card className="mx-auto max-w-md">
+        <CardContent>
+          <div className="space-y-4">
+            <div className="space-y-2" />
+          </div>
+        </CardContent>
+        <CardFooter className="flex justify-center relative">
+          <div className="w-40 h-40 bg-gray-100 flex items-center justify-center">
+            <QRCode
+              value="https://github.com/gcoro/react-qrcode-logo"
+              size={150}
+              bgColor={bgColor}
+              fgColor={fgColor}
+              eyeColor={eyeColor}
+              eyeRadius={eyeRadius}
+              qrStyle={qrStyle}
+              logoImage="path_to_your_logo.png"
+              logoWidth={30} // Adjust as needed
+              logoHeight={30} // Adjust as needed
             />
-          </a>
+          </div>
+        </CardFooter>
+      </Card>
+      <div className="text-center space-y-2">
+        <div className="flex items-center justify-center space-x-2">
+          <Switch
+            id="qr-type"
+            checked={generateForEveryTable}
+            onCheckedChange={handleSwitchChange}
+          />
+          <Label htmlFor="airplane-mode">Multiple QR</Label>
         </div>
+        {generateForEveryTable ? (
+          <Button variant="outline" className="ml-5" type="submit">
+            Generate QR Code For Every Table
+          </Button>
+        ) : (
+          <>
+            <ComboboxDemo />
+            <Button variant="outline" className="ml-5" type="submit">
+              Generate QR Code For Table
+            </Button>
+          </>
+        )}
       </div>
-
-      <div className="relative flex place-items-center before:absolute before:h-[300px] before:w-[480px] before:-translate-x-1/2 before:rounded-full before:bg-gradient-radial before:from-white before:to-transparent before:blur-2xl before:content-[''] after:absolute after:-z-20 after:h-[180px] after:w-[240px] after:translate-x-1/3 after:bg-gradient-conic after:from-sky-200 after:via-blue-200 after:blur-2xl after:content-[''] before:dark:bg-gradient-to-br before:dark:from-transparent before:dark:to-blue-700 before:dark:opacity-10 after:dark:from-sky-900 after:dark:via-[#0141ff] after:dark:opacity-40 before:lg:h-[360px] z-[-1]">
-        <Image
-          className="relative dark:drop-shadow-[0_0_0.3rem_#ffffff70] dark:invert"
-          src="/next.svg"
-          alt="Next.js Logo"
-          width={180}
-          height={37}
-          priority
-        />
+      <div className="text-center space-y-2">
+        <Tabs defaultValue="eyecolor">
+          <TabsList className="grid w-full grid-cols-5">
+            <TabsTrigger value="eyecolor">Eye Color</TabsTrigger>
+            <TabsTrigger value="eyeradius">Eye Radius</TabsTrigger>
+            <TabsTrigger value="qrstyle">QR Style</TabsTrigger>
+            <TabsTrigger value="backgroundcolor">Background Color</TabsTrigger>
+            <TabsTrigger value="foregroundcolor">Foreground Color</TabsTrigger>
+          </TabsList>
+          <TabsContent value="eyecolor">
+            <Card>
+              <CardHeader>
+                <CardTitle>Eye Color</CardTitle>
+                <CardDescription>
+                  Make changes on QR Code Eye Color here
+                </CardDescription>
+              </CardHeader>
+              <CardContent className="space-y-2">
+                <div>
+                  <label>Eye Color:</label>
+                  <input
+                    type="color"
+                    value={eyeColor}
+                    onChange={(e) => setEyeColor(e.target.value)}
+                  />
+                </div>
+              </CardContent>
+            </Card>
+          </TabsContent>
+          <TabsContent value="eyeradius">
+            <Card>
+              <CardHeader>
+                <CardTitle>Eye Radius</CardTitle>
+                <CardDescription>
+                  Make changes on QR Code Eye Radius here
+                </CardDescription>
+              </CardHeader>
+              <CardContent className="space-y-2">
+                <div>
+                  <label>Eye Radius:</label>
+                  <input
+                    type="number"
+                    value={eyeRadius}
+                    onChange={(e) => setEyeRadius(Number(e.target.value))}
+                  />
+                </div>
+              </CardContent>
+            </Card>
+          </TabsContent>
+          <TabsContent value="qrstyle">
+            <Card>
+              <CardHeader>
+                <CardTitle>QR Style</CardTitle>
+                <CardDescription>
+                  Make changes on QR Code Style here
+                </CardDescription>
+              </CardHeader>
+              <CardContent className="space-y-2">
+                <div>
+                  <label>QR Style:</label>
+                  <select
+                    value={qrStyle || ""}
+                    onChange={(e) =>
+                      setQrStyle(e.target.value as "squares" | "dots")
+                    }
+                  >
+                    <option value="squares">Squares</option>
+                    <option value="dots">Dots</option>
+                  </select>
+                </div>
+              </CardContent>
+            </Card>
+          </TabsContent>
+          <TabsContent value="backgroundcolor">
+            <Card>
+              <CardHeader>
+                <CardTitle>Background Color</CardTitle>
+                <CardDescription>
+                  Make changes on Background Color here
+                </CardDescription>
+              </CardHeader>
+              <CardContent className="space-y-2">
+                <div>
+                  <label>Background Color:</label>
+                  <input
+                    type="color"
+                    value={bgColor}
+                    onChange={(e) => setBgColor(e.target.value)}
+                  />
+                </div>
+              </CardContent>
+            </Card>
+          </TabsContent>
+          <TabsContent value="foregroundcolor">
+            <Card>
+              <CardHeader>
+                <CardTitle>Foreground Color</CardTitle>
+                <CardDescription>
+                  Make changes on Foreground Color here
+                </CardDescription>
+              </CardHeader>
+              <CardContent className="space-y-2">
+                <div>
+                  <label>Foreground Color:</label>
+                  <input
+                    type="color"
+                    value={fgColor}
+                    onChange={(e) => setFgColor(e.target.value)}
+                  />
+                </div>
+              </CardContent>
+            </Card>
+          </TabsContent>
+        </Tabs>
       </div>
+    </div>
+  );
+}
+function CameraIcon(props) {
+  return (
+    <svg
+      {...props}
+      xmlns="http://www.w3.org/2000/svg"
+      width="24"
+      height="24"
+      viewBox="0 0 24 24"
+      fill="none"
+      stroke="currentColor"
+      strokeWidth="2"
+      strokeLinecap="round"
+      strokeLinejoin="round"
+    >
+      <path d="M14.5 4h-5L7 7H4a2 2 0 0 0-2 2v9a2 2 0 0 0 2 2h16a2 2 0 0 0 2-2V9a2 2 0 0 0-2-2h-3l-2.5-3z" />
+      <circle cx="12" cy="13" r="3" />
+    </svg>
+  );
+}
 
-      <div className="mb-32 grid text-center lg:max-w-5xl lg:w-full lg:mb-0 lg:grid-cols-4 lg:text-left">
-        <a
-          href="https://nextjs.org/docs?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-          className="group rounded-lg border border-transparent px-5 py-4 transition-colors hover:border-gray-300 hover:bg-gray-100 hover:dark:border-neutral-700 hover:dark:bg-neutral-800/30"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2 className={`mb-3 text-2xl font-semibold`}>
-            Docs{' '}
-            <span className="inline-block transition-transform group-hover:translate-x-1 motion-reduce:transform-none">
-              -&gt;
-            </span>
-          </h2>
-          <p className={`m-0 max-w-[30ch] text-sm opacity-50`}>
-            Find in-depth information about Next.js features and API.
-          </p>
-        </a>
-
-        <a
-          href="https://nextjs.org/learn?utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-          className="group rounded-lg border border-transparent px-5 py-4 transition-colors hover:border-gray-300 hover:bg-gray-100 hover:dark:border-neutral-700 hover:dark:bg-neutral-800/30"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2 className={`mb-3 text-2xl font-semibold`}>
-            Learn{' '}
-            <span className="inline-block transition-transform group-hover:translate-x-1 motion-reduce:transform-none">
-              -&gt;
-            </span>
-          </h2>
-          <p className={`m-0 max-w-[30ch] text-sm opacity-50`}>
-            Learn about Next.js in an interactive course with&nbsp;quizzes!
-          </p>
-        </a>
-
-        <a
-          href="https://vercel.com/templates?framework=next.js&utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-          className="group rounded-lg border border-transparent px-5 py-4 transition-colors hover:border-gray-300 hover:bg-gray-100 hover:dark:border-neutral-700 hover:dark:bg-neutral-800/30"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2 className={`mb-3 text-2xl font-semibold`}>
-            Templates{' '}
-            <span className="inline-block transition-transform group-hover:translate-x-1 motion-reduce:transform-none">
-              -&gt;
-            </span>
-          </h2>
-          <p className={`m-0 max-w-[30ch] text-sm opacity-50`}>
-            Explore starter templates for Next.js.
-          </p>
-        </a>
-
-        <a
-          href="https://vercel.com/new?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-          className="group rounded-lg border border-transparent px-5 py-4 transition-colors hover:border-gray-300 hover:bg-gray-100 hover:dark:border-neutral-700 hover:dark:bg-neutral-800/30"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2 className={`mb-3 text-2xl font-semibold`}>
-            Deploy{' '}
-            <span className="inline-block transition-transform group-hover:translate-x-1 motion-reduce:transform-none">
-              -&gt;
-            </span>
-          </h2>
-          <p className={`m-0 max-w-[30ch] text-sm opacity-50`}>
-            Instantly deploy your Next.js site to a shareable URL with Vercel.
-          </p>
-        </a>
-      </div>
-    </main>
-  )
+function QrCodeIcon(props) {
+  return (
+    <svg
+      {...props}
+      xmlns="http://www.w3.org/2000/svg"
+      width="24"
+      height="24"
+      viewBox="0 0 24 24"
+      fill="none"
+      stroke="currentColor"
+      strokeWidth="2"
+      strokeLinecap="round"
+      strokeLinejoin="round"
+    >
+      <rect width="5" height="5" x="3" y="3" rx="1" />
+      <rect width="5" height="5" x="16" y="3" rx="1" />
+      <rect width="5" height="5" x="3" y="16" rx="1" />
+      <path d="M21 16h-3a2 2 0 0 0-2 2v3" />
+      <path d="M21 21v.01" />
+      <path d="M12 7v3a2 2 0 0 1-2 2H7" />
+      <path d="M3 12h.01" />
+      <path d="M12 3h.01" />
+      <path d="M12 16v.01" />
+      <path d="M16 12h1" />
+      <path d="M21 12v.01" />
+      <path d="M12 21v-1" />
+    </svg>
+  );
 }
